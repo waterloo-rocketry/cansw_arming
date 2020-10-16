3b5c3926f86dad14c8ecf842e0a3d21dbad1114c
Zachariah Mears <zx100x100@gmail.com>
2020-10-16 01:04:42 -0400

    Create error_checks.h and .c, include in main with boiler plate code.

---
+++ error_checks.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_CHECKS_H
+#define	ERROR_CHECKS_H
+
+#include "canlib/message_types.h"
+#include <stdbool.h>
+
+bool check_battery_voltage_error(void);
+
+#endif	/* ERROR_CHECKS_H */
+
